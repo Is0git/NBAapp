48bc4327ac0b7eceaad5180b22ac1b27a6f2db2e
Danielius <dnlspasin@gmail.com>
2019-10-16 12:39:49 +0300

    navigation + homefragment layout

---
+++ app/src/main/java/com/android/nbaapp/ui/fragments/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.android.nbaapp.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.android.nbaapp.databinding.HomeFragmentBinding
+
+class HomeFragment : Fragment() {
+    lateinit var binding:HomeFragmentBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = HomeFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}

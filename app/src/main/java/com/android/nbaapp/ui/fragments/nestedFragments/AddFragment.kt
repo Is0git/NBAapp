85174c101605d02f70459e15bce3ff83e245f29f
Danielius <dnlspasin@gmail.com>
2019-10-22 18:51:08 +0300

    Add news with images

--- app/src/main/java/com/android/nbaapp/ui/fragments/nestedFragments/AddFragment.kt
+++ app/src/main/java/com/android/nbaapp/ui/fragments/nestedFragments/AddFragment.kt
@@ -1,11 +1,15 @@
 package com.android.nbaapp.ui.fragments.nestedFragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.android.nbaapp.data.db.enitities.NewsEntity
 import com.android.nbaapp.data.vms.AddFragmentViewmodel
 import com.android.nbaapp.data.vms.ViewModelFactory
 import com.android.nbaapp.databinding.AddFragmentBinding
@@ -23,7 +27,9 @@ class AddFragment : DaggerFragment(){
         savedInstanceState: Bundle?
     ): View? {
         binding = AddFragmentBinding.inflate(inflater,container, false)
-
+        viewmodel = ViewModelProviders.of(activity!!, viewmodelFactory)
+            .get(AddFragmentViewmodel::class.java)
+        binding.add.setOnClickListener { onAddClick(it) }
         return binding.root
     }
 
@@ -32,11 +38,13 @@ class AddFragment : DaggerFragment(){
     }
 
     fun onAddClick(view: View) {
-        if (binding.titleText.text.toString().isNotBlank()
-            && binding.descriptionText.text.toString().isNotBlank()
-            && binding.imageUrlText.text.toString().isNotBlank()
-        ) {
-
-        }
+        Log.d("TAG", "TRIGGERED")
+        val title = binding.titleText.text.toString()
+        val description = binding.descriptionText.text.toString()
+        val image_url = binding.imageUrlText.text.toString()
+        if (title.isNotBlank() && description.isNotBlank() && image_url.isNotBlank()) viewmodel.addNews(
+            NewsEntity(title, image_url, description)
+        ).also { navigation.navigateUp() }
+        else Toast.makeText(context, "FILL ALL FIELDS", Toast.LENGTH_LONG).show()
     }
 }
---
+++ app/src/main/java/com/android/nbaapp/utils/MyHandlers.kt
@@ -0,0 +1,8 @@
+package com.android.nbaapp.utils
+
+import android.util.Log
+import android.view.View
+
+class MyHandlers {
+    fun onClickFriend(view: View) { Log.d("TAG", "SUMOETING") }
+}
